c2aa5e48bd29d312c7e9cc125b3b9a7f2d1163a9
Roger Gomez <roger@techknob.com>
2020-06-30 18:44:47 -0400

    adding more instances and the state

--- main.tf
+++ main.tf
@@ -4,8 +4,21 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  count = 2
+  count = 3
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
 
+output "my_output" {
+  value = aws_instance.example[*].id
+}
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "hallownest"
+    workspaces {
+      name = "tf-exam-test"
+    }
+  }
+}
